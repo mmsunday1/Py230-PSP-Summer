b2a5cce41853909fbc04519256cce38c376225af
mmsunday1 <matt.m.sunday@gmail.com>
2019-02-23 15:43:17 -0800

    flask/to-do exercise

---
+++ Student/M_Sunday/Lesson01/flask-todo/setup.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+from model import db, User, Task
+from passlib.hash import pbkdf2_sha256
+
+db.connect()
+db.drop_tables([User, Task])
+db.create_tables([User, Task])
+
+Task(name="Do the Laundry.").save()
+Task(name="Do the dishes.", performed=datetime.now()).save()
+User(name="admin", password=pbkdf2_sha256.hash("password")).save()
+User(name="bob", password=pbkdf2_sha256.hash("bobbob")).save()
